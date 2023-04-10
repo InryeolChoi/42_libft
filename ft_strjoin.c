ba8c6742b6c4a6f51039b94aef84e6abb42e278f
Inryeol Choi <inchoi@cx1r4s5.42seoul.kr>
2023-04-10 17:37:18 +0900

    edit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,20 +18,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	i = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s1)
+		return (ft_strdup((char *)s2));
+	else if (!s2)
+		return (ft_strdup((char *)s1));
 	size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	joined = (char *)malloc(size * sizeof(char));
 	if (!joined)
 		return (0);
 	while (i < size && *s1 != '\0')
-	{
-		joined[i++] = *s1;
-		s1++;
-	}
+		joined[i++] = *(s1++);
 	while (i < size && *s2 != '\0')
-	{
-		joined[i++] = *s2;
-		s2++;
-	}
+		joined[i++] = *(s2++);
 	joined[i] = '\0';
 	return (joined);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -25,7 +25,7 @@ static int	in(char x, const char *set)
 	return (0);
 }
 
-static char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	l;
 	size_t	r;
